22941e48d598af7f32d1d30846cdcdf8873dca8f
DraynaRue <mattie0@hotmail.co.uk>
2018-10-17 10:30:12 +0100

    Imported Whitebox and Shaders

---
+++ MovementController/Assets/UI/Fuel.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Fuel : MonoBehaviour {
+
+	public GameObject fuelGage;
+	public float fuelPercentage = 100f;
+	public Animator animator;
+
+	void Start(){
+		animator.Play("fuel", -1, fuelPercentage);
+	}
+
+	void Update(){
+		if(Input.GetKey(KeyCode.Space) && fuelPercentage > 0){
+			fuelPercentage -= 2.5f * Time.deltaTime;
+			animator.Play("fuel", -1, (1f / 100) * fuelPercentage);
+		} else if(fuelPercentage < 100){
+			fuelPercentage += 2f * Time.deltaTime;
+			animator.Play("fuel", -1, (1f / 100) * fuelPercentage);
+		} else{
+			animator.speed = 0f;
+		}
+			
+	}
+}
