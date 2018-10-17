da42fbffe41766c8b8d2b3875fee3b38b1d9bc78
DraynaRue <mattie0@hotmail.co.uk>
2018-10-17 12:59:20 +0100

    Fuel only depletes when using jetpack

--- MovementController/Assets/UI/Fuel.cs
+++ MovementController/Assets/UI/Fuel.cs
@@ -5,9 +5,11 @@ using UnityEngine.UI;
 
 public class Fuel : MonoBehaviour
 {
-
 	public GameObject fuelGage;
+	public GameObject player;
 	public float fuelPercentage = 100f;
+
+	public bool isOutOfFuel = false;
 	public Animator animator;
 
 	void Start()
@@ -17,9 +19,11 @@ public class Fuel : MonoBehaviour
 
 	void Update()
 	{
-		if (GetComponent<MovementScript>().isZeroGravity == true)
+		// Checks if zero gravity mode is enabled
+		if (player.GetComponent<MovementScript>().isZeroGravity == true)
 		{
-			if(Input.GetKey(KeyCode.Space) && fuelPercentage > 0)
+			// Checks to see if the 'jetpack' is being used
+			if(Input.GetAxis("Horizontal") != 0 && fuelPercentage > 0 || Input.GetAxis("Vertical") != 0 && fuelPercentage > 0 || Input.GetAxis("Jump") != 0 && fuelPercentage > 0 || Input.GetAxis("Crouch") != 0 && fuelPercentage > 0 )
 			{
 				fuelPercentage -= 2.5f * Time.deltaTime;
 				animator.Play("fuel", -1, (1f / 100) * fuelPercentage);
@@ -28,6 +32,12 @@ public class Fuel : MonoBehaviour
 		else
 		{
 			animator.speed = 0f;
+		}
+
+		// Sets isOutOfFuel to true if fuelPercentage is at 0
+		if (fuelPercentage <= 0)
+		{
+			isOutOfFuel = true;
 		}
 	}
 }
