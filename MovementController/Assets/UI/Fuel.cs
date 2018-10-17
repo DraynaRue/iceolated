8fcd7c716e07df8f6e9a8d776c5911e9bb95098c
DraynaRue <mattie0@hotmail.co.uk>
2018-10-17 12:06:27 +0100

    Began modifying fuel script

--- MovementController/Assets/UI/Fuel.cs
+++ MovementController/Assets/UI/Fuel.cs
@@ -3,26 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Fuel : MonoBehaviour {
+public class Fuel : MonoBehaviour
+{
 
 	public GameObject fuelGage;
 	public float fuelPercentage = 100f;
 	public Animator animator;
 
-	void Start(){
+	void Start()
+	{
 		animator.Play("fuel", -1, fuelPercentage);
 	}
 
-	void Update(){
-		if(Input.GetKey(KeyCode.Space) && fuelPercentage > 0){
-			fuelPercentage -= 2.5f * Time.deltaTime;
-			animator.Play("fuel", -1, (1f / 100) * fuelPercentage);
-		} else if(fuelPercentage < 100){
-			fuelPercentage += 2f * Time.deltaTime;
-			animator.Play("fuel", -1, (1f / 100) * fuelPercentage);
-		} else{
+	void Update()
+	{
+		if (GetComponent<MovementScript>().isZeroGravity == true)
+		{
+			if(Input.GetKey(KeyCode.Space) && fuelPercentage > 0)
+			{
+				fuelPercentage -= 2.5f * Time.deltaTime;
+				animator.Play("fuel", -1, (1f / 100) * fuelPercentage);
+			}
+		}
+		else
+		{
 			animator.speed = 0f;
 		}
-			
 	}
 }
