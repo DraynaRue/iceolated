07056c07bbc971f4defc2a306e6d0b63ae2a29ce
Clayton <CC1CRT@Bolton.ac.uk>
2018-12-12 12:46:12 +0000

    ModPlacement 100% | Clean-up
    Added window, pipes, posters, fixed sobel outlines.
    
    Merged all > Master

---
+++ MovementController/Assets/Scripts/Explosion.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion : MonoBehaviour {
+
+	public float radius, power;
+
+	void Start () {
+		Vector3 pos = transform.position;
+		Collider[] colliders = Physics.OverlapSphere(pos, radius);
+
+        foreach (Collider hit in colliders){
+            Rigidbody rb = hit.GetComponent<Rigidbody>();
+			
+            if (rb != null)
+                rb.AddExplosionForce(power, pos, radius, 3.0F);
+        }
+    }
+}
---
+++ MovementController/Assets/Scripts/InteractScript.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractScript : MonoBehaviour {
+
+	public GameObject UIElement;
+	public GameObject Player;
+
+	// Use this for initialization
+	void Start ()
+	{
+		UIElement.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == "Player")
+		{
+			UIElement.SetActive(true);
+		}
+	}
+
+	private void OnTriggerStay(Collider other)
+	{
+		if (other.tag == "Player")
+		{
+			if (this.GetComponent<KeyScript>() != null && Input.GetButton("Interact") == true)
+			{
+				GetComponent<KeyScript>().Interact();
+				UIElement.SetActive(false);
+			}
+			else if (this.GetComponent<AudioLogScript>() != null && Input.GetButton("Interact") == true)
+			{
+				GetComponent<AudioLogScript>().Interact();
+				UIElement.SetActive(false);
+			}
+			else if (this.GetComponent<TeleporterScript>() != null && Input.GetButton("Interact") == true )
+			{
+ 				GetComponent<TeleporterScript>().Interact();
+			}
+		}
+	}
+}
