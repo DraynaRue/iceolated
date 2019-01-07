227cb69aec5f69f365ea021d0675ad452e045d5c
DraynaRue <mattie0@hotmail.co.uk>
2019-01-07 12:39:36 +0000

    Randomised word order

--- MovementController/Assets/Scripts/IceMelt.cs
+++ MovementController/Assets/Scripts/IceMelt.cs
@@ -2,19 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class IceMelt : MonoBehaviour {
+public class IceMelt : MonoBehaviour 
+{
 
 	public GameObject laser;
 
-	void Update () {
+	void Update () 
+	{
 		RaycastHit hit;
 
-		if(Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity)){
-			if(hit.transform.gameObject.tag == "Ice" && Input.GetKey(KeyCode.F)){
+		if(Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity)) 
+		{
+			if(hit.transform.gameObject.tag == "Ice" && Input.GetKey(KeyCode.F)) 
+			{
 				Debug.Log("ok hit ice");
 				laser.gameObject.SetActive(true);
-				hit.transform.localScale -= Vector3.one * Time.deltaTime * 0.1f;
-			} else{
+				hit.transform.localScale -= Vector3.right * Time.deltaTime * 0.1f;
+				if (hit.transform.localScale.x <= 0.00f)
+				{
+					Destroy(hit.transform.gameObject);
+				}
+			} 
+			else 
+			{
 				laser.gameObject.SetActive(false);
 			}
 		}
--- MovementController/Assets/Scripts/Terminal.cs
+++ MovementController/Assets/Scripts/Terminal.cs
@@ -3,36 +3,63 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Terminal : MonoBehaviour {
+public class Terminal : MonoBehaviour
+{
 
 	public GameObject terminalInterface;
 	public CameraScript camScript;
+	public MovementScript movScript;
 	public GameObject player;
+	public GameObject cam;
 	public Text A1, A2, A3, A4, A5, A6, A7, A8, A9, A10,
 				B1, B2, B3, B4, B5, B6, B7, B8, B9, B10,
 				C1, C2, C3, C4, C5, C6, C7, C8, C9, C10;
 
-	public string 	twist, aware, mayor, swarm, smell, video,
-	 			 	blast, shave, youth, peace, shift, donor, awful, tough,
-	  				hobby, wheel, style, tight, drown, abuse, stick, sweet,
-	  				elect, brave, split, crime, clerk, penny, tribe, pound;
+	protected string twist="twist", aware="aware", mayor="mayor", swarm="swarm", smell="smell", video="video", blast="blast", shave="shave", youth="youth", peace="peace", 
+					 shift="shift", donor="donor", awful="awful", tough="tough", hobby="hobby", wheel="wheel", style="style", tight="tight", drown="drown", abuse="abuse", 
+					 stick="stick", sweet="sweet", elect="elect", brave="brave", split="split", crime="crime", clerk="clerk", penny="penny", tribe="tribe", pound="pound";
+	protected List<Text> TextArray;
+	protected List<string> WordArray; 
 
-	void Start(){
-		player = GameObject.FindGameObjectWithTag("MainCamera");
-		camScript = player.GetComponent<CameraScript>();
+	void Start()
+	{
+		player = GameObject.FindGameObjectWithTag("Player");
+		movScript = player.GetComponent<MovementScript>();
+
+		cam = GameObject.FindGameObjectWithTag("MainCamera");
+		camScript = cam.GetComponent<CameraScript>();
 	}
-	void OnTriggerStay(Collider other){
-		if(other.gameObject.tag == "Player" && Input.GetKeyDown(KeyCode.Return)){
+	void OnTriggerEnter(Collider other) 
+	{
+		TextArray = new List<Text> {A1, A2, A3, A4, A5, A6, A7, A8, A9, A10,
+									B1, B2, B3, B4, B5, B6, B7, B8, B9, B10,
+									C1, C2, C3, C4, C5, C6, C7, C8, C9, C10};
+		WordArray = new List<string> {twist, aware, mayor, swarm, smell, video, blast, shave, youth, peace,
+								      shift, donor, awful, tough, hobby, wheel, style, tight, drown, abuse, 
+									  stick, sweet, elect, brave, split, crime, clerk, penny, tribe, pound};
+		for (int i = 0; i <= TextArray.Count; i++)
+		{
+			TextArray[i].text = WordArray[Random.Range(0, WordArray.Count)];
+		}
+	}
+	void OnTriggerStay(Collider other) 
+	{
+		if(other.gameObject.tag == "Player" && Input.GetKeyDown(KeyCode.Return))
+		{
 			terminalInterface.SetActive(true);
 			camScript.enabled = false;
-			Cursor.lockState = CursorLockMode.None;	
+			movScript.enabled = false;
+			Cursor.lockState = CursorLockMode.None;
 		}
 	}
 
-	void OnTriggerExit(Collider other){
-		if(other.gameObject.tag == "Player"){
+	void OnTriggerExit(Collider other)
+	{
+		if(other.gameObject.tag == "Player")
+		{
 			terminalInterface.SetActive(false);
 			camScript.enabled = true;
+			movScript.enabled = true;
 			Cursor.lockState = CursorLockMode.Confined;
 			Cursor.lockState = CursorLockMode.Locked;
 		}
