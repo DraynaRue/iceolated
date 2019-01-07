a4b255fb859b3f8b3c7a0db76702744e42959d38
Mika Joel <me@mikajoel.fyi>
2019-01-07 10:09:57 +0000

    Take 2

---
+++ MovementController/Assets/Scripts/IceMelt.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IceMelt : MonoBehaviour {
+
+	public GameObject laser;
+
+	void Update () {
+		RaycastHit hit;
+
+		if(Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity)){
+			if(hit.transform.gameObject.tag == "Ice" && Input.GetKey(KeyCode.F)){
+				Debug.Log("ok hit ice");
+				laser.gameObject.SetActive(true);
+				hit.transform.localScale -= Vector3.one * Time.deltaTime * 0.1f;
+			} else{
+				laser.gameObject.SetActive(false);
+			}
+		}
+	}
+}
