50738055f2c0155cb9678d2d250d025a87490ae3
Mika Joel <me@mikajoel.fyi>
2019-01-08 17:30:55 +0000

    Puzzle 2 Started

--- MovementController/Assets/Scripts/TimerScript.cs
+++ MovementController/Assets/Scripts/TimerScript.cs
@@ -6,23 +6,36 @@ using System.Threading;
 
 public class TimerScript : MonoBehaviour
 {
-	public int timeLeft = 60;
-	public Text countdown; 
-    void Start ()
-	{
-		StartCoroutine("LoseTime");
-		Time.timeScale = 1; 
-	}
-   void Update ()
-   {
-     countdown.text = ("" + timeLeft);
-   }
-    IEnumerator LoseTime()
-    {
-	    while (true)
-		{
-          yield return new WaitForSeconds (1);
-          timeLeft--;
+	public float timeLeft = 60f;
+	public Text countdown;
+	public GameObject timerObj;
+	public bool TimerTrigger;
+
+	public Text tQTE1, tQTE2, tQTE3, tQTE4, tQTE5, tQTE6;
+
+
+	void OnTriggerEnter(Collider other){
+		if(other.gameObject.tag == "Player"){
+			TimerTrigger = true;
 		}
 	}
+	
+	void Update(){
+		if(TimerTrigger){
+			timerObj.SetActive(true);
+			countdown.text = "" + timeLeft.ToString();
+			timeLeft -= 1 * Time.deltaTime;
+
+			if(timeLeft <= 0){
+				Debug.Log("Engines Blew Up!");
+			}
+
+			QTE();
+		}
+	}
+
+	public void QTE(){
+		
+	}
+
 }
