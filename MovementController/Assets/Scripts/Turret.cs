c98f1dd587897ee5a25873e28b6a6212c96ebe7d
s <sd>
2019-01-30 12:10:06 +0000

    Turret Implemented and Rotates

---
+++ MovementController/Assets/Scripts/Turret.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turret : MonoBehaviour {
+
+	public Transform target;
+
+
+
+	void Update(){
+		Vector3 targetDir = target.position - transform.position;
+
+		Vector3 newDir = Vector3.RotateTowards(transform.forward, targetDir, 4f, 0f);
+		Debug.DrawRay(transform.position, newDir, Color.red);
+		transform.rotation = Quaternion.LookRotation(newDir);
+
+		
+	}
+}
