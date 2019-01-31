53de955a9dc662c54c369c0900ec9e4105c34ce6
Joel <Joel>
2019-01-31 11:59:27 +0000

    Turret Shoots

---
+++ MovementController/Assets/_Scripts/Turret/Bullet.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+	private Rigidbody rb;
+
+	void Start(){
+		rb = GetComponent<Rigidbody>();
+	}
+
+	void Update(){
+		rb.AddForce(Vector3.forward, ForceMode.Force);
+	}
+
+	void OnTriggerEnter(Collider other){
+		if(other.gameObject.tag == "Player"){
+			PlayerHealth.health -= 10f;
+			
+		}		
+	}
+}
---
+++ MovementController/Assets/_Scripts/Turret/Turret.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turret : MonoBehaviour {
+
+	public Transform target;
+	public GameObject bullet;
+	private bool isShooting = false;
+
+
+	void Update(){
+		Vector3 targetDir = target.position - transform.position;
+
+		Vector3 newDir = Vector3.RotateTowards(transform.forward, targetDir, 4f, 0f);
+		Debug.DrawRay(transform.position, newDir, Color.red);
+		transform.rotation = Quaternion.LookRotation(newDir);
+
+		RaycastHit hit;
+
+		if(Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity)) 
+		{
+			if(hit.transform.gameObject.tag == "Player" && isShooting == false){
+				StartCoroutine(SHOOTING());			
+			}
+		}
+	}
+
+	IEnumerator SHOOTING (){
+		isShooting = true;
+		Instantiate(bullet, transform.position, Quaternion.identity);
+		yield return new WaitForSeconds(2f);
+		isShooting = false;
+	}
+}
