db0d811345a860851fa0ca859167585c9884846c
marcus <s>
2019-01-31 12:43:11 +0000

    Main Menu and Pause

---
+++ MovementController/Assets/_Scripts/AudioManager.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour {
+
+	public enum AudioChannel {Master, Sfx, Music}
+
+	public float masterVolumePercent { get; private set; }
+	public float sfxVolumePercent { get; private set; }
+    public float musicVolumePercent { get; private set; }
+
+    AudioSource [] musicSources;
+	int activeMusicSourceIndex;
+
+	public static AudioManager Instance;
+
+	Transform audioListener;
+	Transform playerT;
+
+	SoundLibrary library;
+
+	void Awake(){
+		if (Instance != null) {
+			Destroy (gameObject);
+		}
+		else{
+			Instance = this;
+			DontDestroyOnLoad (gameObject);
+			musicSources = new AudioSource[2];
+			for (int i =0; i < 2; i++){
+				GameObject newMusicSource = new GameObject ("Music Source " + (i + 1));
+				musicSources [i] = newMusicSource.AddComponent <AudioSource> ();
+				newMusicSource.transform.parent = transform;
+			}
+			audioListener = FindObjectOfType <AudioListener> ().transform;
+           // if (FindObjectOfType<Player>() != null) { 
+			   // playerT = FindObjectOfType <Player> ().transform;
+            //}
+			library = GetComponent <SoundLibrary>();
+
+			masterVolumePercent = PlayerPrefs.GetFloat ("master vol", 1);
+			sfxVolumePercent = PlayerPrefs.GetFloat ("sfx vol", 1);
+			musicVolumePercent = PlayerPrefs.GetFloat ("music vol", 1);
+		}
+	}
+
+	void Update (){
+		//if (playerT != null) {
+		//	audioListener.position = playerT.postion;
+		//}
+	}
+
+	public void SetVolume (float volumePercent, AudioChannel channel){
+		switch (channel) { 
+		case AudioChannel.Master:
+			masterVolumePercent = volumePercent;
+			break;
+		case AudioChannel.Sfx:
+			sfxVolumePercent = volumePercent;
+			break;
+		case AudioChannel.Music:
+			musicVolumePercent = volumePercent;
+			break;
+		}
+		musicSources [0].volume = musicVolumePercent * masterVolumePercent;
+		musicSources [1].volume = musicVolumePercent * masterVolumePercent;
+
+		PlayerPrefs.SetFloat ("master vol", masterVolumePercent);
+		PlayerPrefs.SetFloat ("sfx vol", sfxVolumePercent);
+		PlayerPrefs.SetFloat ("music vol", musicVolumePercent);
+        PlayerPrefs.Save ();
+	}
+
+	public void PlayMusic (AudioClip AudioClip, float fadeDuration =1){
+		activeMusicSourceIndex = 1 - activeMusicSourceIndex;
+		musicSources [activeMusicSourceIndex].clip = AudioClip;
+		musicSources [activeMusicSourceIndex].Play ();
+		StartCoroutine (AnimateMusicCrossfade (fadeDuration)); 
+	}
+
+	//public void PlaySound (string soundName, Vector3 pos){
+	//	PlaySound (library.GetClipFromName (soundName), pos);
+	//}
+
+
+
+	IEnumerator AnimateMusicCrossfade(float duration){
+		float percent = 0;
+
+		while (percent < 1) {
+			percent += Time.deltaTime * 1 / duration; 
+			musicSources [activeMusicSourceIndex].volume = Mathf.Lerp (0, musicVolumePercent * masterVolumePercent, percent); 
+			musicSources [1-activeMusicSourceIndex].volume = Mathf.Lerp (musicVolumePercent * masterVolumePercent, 0, percent); 
+			yield return null;
+		}
+	}
+}
