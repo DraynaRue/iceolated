bf96e503ca1b34f3caf7a3aa2cfd1a90bccd4882
DraynaRue <mattie0@hotmail.co.uk>
2019-01-31 14:53:52 +0000

    Created drone
    Bugs: Gets stuck randomly

---
+++ MovementController/Assets/_Scripts/DroneScript.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DroneScript : MonoBehaviour 
+{
+	public GameObject Target;
+	public float SightDistance;
+	public float HoverHeight;
+	public float VerticalSpeed;
+	public float HorizonalSpeed;
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		Hover();
+
+		Vector3 targetDir = Target.transform.position - transform.position;
+		float angle = Vector3.Angle(targetDir, transform.forward);
+		float dist = Vector3.Distance(Target.transform.position, transform.position);
+		if (angle <= 60.0f && dist <= SightDistance) 
+		{
+			Quaternion rot = Quaternion.LookRotation(targetDir, Vector3.up);
+			rot.x = 0;
+			rot.z = 0;
+			transform.rotation = Quaternion.Slerp(transform.rotation, rot, Time.deltaTime * 10f);
+
+			RaycastHit hit;
+			if (Physics.Raycast(transform.position, transform.forward, out hit, Mathf.Infinity))
+			{
+				float bufferDist = 0.5f;
+				if (hit.distance >= bufferDist - 0.01f && hit.distance <= bufferDist + 0.01f)
+				{
+					// Stops jittering
+				}
+				else if (hit.distance <= bufferDist)
+				{
+					transform.position += transform.forward * -HorizonalSpeed;
+				}
+				else
+				{
+					transform.position += transform.forward * HorizonalSpeed;
+				}
+			}
+		}
+	}
+
+	void Hover ()
+	{
+		RaycastHit hit;
+		Debug.DrawRay(transform.position, Vector3.down * 10, Color.red);
+		if (Physics.Raycast(transform.position, Vector3.down, out hit, Mathf.Infinity))
+		{
+			if (hit.distance >= HoverHeight - 0.01f && hit.distance <= HoverHeight + 0.01f)
+			{
+				//stops jittering
+			}
+			else if (hit.distance <= HoverHeight)
+			{
+				transform.position += Vector3.up * VerticalSpeed;
+			}
+			else if (hit.distance >= HoverHeight)
+			{
+				transform.position += Vector3.up * -VerticalSpeed;
+			}
+		}
+	}
+}
