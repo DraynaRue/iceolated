efef2d86e199c673dcf8212856f29acf9b953bb7
ik <k>
2019-02-12 10:49:01 +0000

    Player Health bar added and player dies

--- MovementController/Assets/_Scripts/Turret/PlayerHealth.cs
+++ MovementController/Assets/_Scripts/Turret/PlayerHealth.cs
@@ -1,13 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour {
 
 	public static float health = 100f;
+	public Text tHealth;
+	public Slider sHealth;
 
 	void Update(){
-		//Debug.Log(health);
+		tHealth.text = health.ToString() + " / 100";
+		sHealth.value = health;
+				
 		if(health <= 0){
 			Debug.Log("Player Dead");
 		}
