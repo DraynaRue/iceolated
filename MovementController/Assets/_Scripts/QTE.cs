d635cf14494f6948cfed608ad1e79afcf2c67431
Mika Joel <me@mikajoel.fyi>
2019-02-20 01:38:27 +0000

    Improved QTE system and fixed bugs,

--- MovementController/Assets/_Scripts/QTE.cs
+++ MovementController/Assets/_Scripts/QTE.cs
@@ -10,114 +10,223 @@ public class QTE : MonoBehaviour {
 	public GameObject qteKeyObj;
 	public GameObject timerTriggerObj;
 	public MovementScript player;
-	public bool QTE1Start;
+	public bool QTEPlaying;
 	public Text QTEKey;
 	public Text QTENum;
 
-	private float QTE1, QTE2, QTE3, QTE4, QTE5, QTE6, QTE7, QTE8;
-	private bool done1, done2, done3, done4, done5, done6, done7, done8;
+	private float QTE1 = 2f, QTE2 = 1.75f, QTE3 = 1.5f, QTE4 = 1.25f, QTE5 = 1f, QTE6 = 0.75f, QTE7 = 0.5f;
+	private bool done1, done2, done3, done4, done5, done6, done7;
 
 	void Update () {
-		if(QTE1Start == true && done1 == false){
-			player.enabled = false;
+		if(QTEPlaying == false){
+			done2 = false;
+			done3 = false;
+			done4 = false;
+			done5 = false;
+			done6 = false;
+			done7 = false;
 
-			QTE1 = (TimerScript.timeLeft / 100f);
-			QTENum.text = QTE1.ToString();
-			QTEKey.text = "Up";
-			qteNumObj.SetActive(true);
-			qteKeyObj.SetActive(true);
-			if(Input.GetAxis("Vertical") > 0 && QTE1 > 0f){
-				done1 = true;
-			}
+			QTE1 = 2f; QTE2 = 1.75f; QTE3 = 1.5f; QTE4 = 1.4f; QTE5 = 1.3f; QTE6 = 1.2f; QTE7 = 1f;
 		}
 
-		if(done1 == true && done2 == false){
-			QTE2 = (TimerScript.timeLeft / 120f);
-			QTENum.text = QTE2.ToString();
-			QTEKey.text = "Down";
-			if(Input.GetAxis("Vertical") < 0 && QTE2 > 0f){
-				done2 = true;
-			}
+		if(QTEPlaying == true){
+			E1();
+			QTE1 -= 0.9f * Time.deltaTime;
 		}
 
-		if(done2 == true && done3 == false){
-			QTE3 = (TimerScript.timeLeft / 140f);
-			QTENum.text = QTE3.ToString();
-			QTEKey.text = "Right";
-			if(Input.GetAxis("Horizontal") > 0 && QTE3 > 0f){
-				done3 = true;
-			}
+		if(done1 == true){
+			E2();
+			QTE2 -= 0.9f * Time.deltaTime;
 		}
 
-		if(done3 == true && done4 == false){
-			QTE4 = (TimerScript.timeLeft / 160f);
-			QTENum.text = QTE4.ToString();
-			QTEKey.text = "Down";
-			if(Input.GetAxis("Vertical") < 0 && QTE4 > 0f){
-				done4 = true;
-			}
+		if(done2 == true){
+			done1 = false;
+			E3();
+			QTE3 -= 0.9f * Time.deltaTime;
 		}
 
-		if(done4 == true && done5 == false){
-			QTE5 = (TimerScript.timeLeft / 180f);
-			QTENum.text = QTE5.ToString();
-			QTEKey.text = "Left";
-			if(Input.GetAxis("Horizontal") < 0 && QTE5 > 0f){
-				done5 = true;
-			}
+		if(done3 == true){
+			done2 = false;
+			E4();
+			QTE4 -= 0.9f * Time.deltaTime;
 		}
 
-		if(done5 == true && done6 == false){
-			QTE6 = (TimerScript.timeLeft / 200f);
-			QTENum.text = QTE6.ToString();
-			QTEKey.text = "Down";
-			if(Input.GetAxis("Vertical") < 0 && QTE6 > 0f){
-				done6 = true;
-			}
+		if(done4 == true){
+			done3 = false;
+			E5();
+			QTE5 -= 0.9f * Time.deltaTime;
 		}
 
-		if(done6 == true && done7 == false){
-			QTE7 = (TimerScript.timeLeft / 220f);
-			QTENum.text = QTE7.ToString();
-			QTEKey.text = "Left";
-			if(Input.GetAxis("Horizontal") < 0 && QTE7 > 0f){
-				done7 = true;
-			}
+		if(done5 == true){
+			done4 = false;
+			E6();
+			QTE6 -= 0.9f * Time.deltaTime;
 		}
 
-		if(done7 == true && done8 == false){
-			QTE8 = (TimerScript.timeLeft / 240f);
-			QTENum.text = QTE8.ToString();
-			QTEKey.text = "Up";
-			if(Input.GetAxis("Vertical") > 0 && QTE8 > 0f){
-				done8 = true;
-			}
+		if(done6 == true){
+			done5 = false;
+			E7();
+			QTE7 -= 0.9f * Time.deltaTime;
 		}
 
-		if(done8){
+		if(done7 == true){
 			player.enabled = true;
-			Destroy(timerTriggerObj);
 			qteNumObj.SetActive(false);
 			qteKeyObj.SetActive(false);
-			TimerScript.timeLeft = 0f;
 		}
 	}
 
-	public void OnTriggerStay(Collider other)
-	{
-		if(other.gameObject.tag == "Player")
-		{
-			interactUI.SetActive(true);
+	void E1(){
+		player.enabled = false;
+		qteNumObj.SetActive(true);
+		qteKeyObj.SetActive(true);
+
+		QTENum.text = QTE1.ToString();
+		QTEKey.text = "[D]";
+		
+		if(Input.GetKeyDown(KeyCode.D) && QTE1 > 0f){ //Win
+			done1 = true;
+		} 
+		
+		if(Input.GetKeyDown(KeyCode.D) && QTE1 <= 0f){ //Fail
+			QTEKey.text = "Fail, try again... Press 'E'";
+			QTEPlaying = false;
+			if(Input.GetKeyDown(KeyCode.E)){
+				done1 = false;
+				QTEPlaying = true;
+			}
+		}	
+	}
+
+	void E2(){
+		QTENum.text = QTE2.ToString();
+		QTEKey.text = "[K]";
+
+		if(Input.GetKeyDown(KeyCode.K) && QTE2 > 0f){
+			done2 = true;
+		}
+
+		if(Input.GetKeyDown(KeyCode.K) && QTE2 <= 0f){
+			QTEKey.text = "Fail, try again... Press 'E'";
+			QTEPlaying = false;
+			if(Input.GetKeyDown(KeyCode.E)){
+				done1 = false;
+				QTEPlaying = true;
+			}
+		}
+	}
+
+	void E3(){
+		QTENum.text = QTE3.ToString();
+		QTEKey.text = "[G]";
+
+		if(Input.GetKeyDown(KeyCode.G) && QTE3 > 0f){
+			done3 = true;
+		}
+
+		if(Input.GetKeyDown(KeyCode.G) && QTE3 <= 0f){
+			QTEKey.text = "Fail, try again... Press 'E'";
+			QTEPlaying = false;
+			if(Input.GetKeyDown(KeyCode.E)){
+				done1 = false;
+				QTEPlaying = true;
+			}
+		}
+	}
+
+	void E4(){
+		QTENum.text = QTE4.ToString();
+			QTEKey.text = "[L]";
+
+			if(Input.GetKeyDown(KeyCode.L) && QTE4 > 0f){
+				done4 = true;
+			}
+
+			if(Input.GetKeyDown(KeyCode.L) && QTE4 <= 0f){
+				QTEKey.text = "Fail, try again... Press 'E'";
+				QTEPlaying = false;
+				if(Input.GetKeyDown(KeyCode.E)){
+					done1 = false;
+					QTEPlaying = true;
+				}
+			}
+	}
+
+	void E5(){
+
+		QTENum.text = QTE5.ToString();
+		QTEKey.text = "[Z]";
+
+		if(Input.GetKeyDown(KeyCode.Z) && QTE5 > 0f){
+			done5 = true;
+		}
+
+		if(Input.GetKeyDown(KeyCode.Z) && QTE5 <= 0f){
+			QTEKey.text = "Fail, try again... Press 'E'";
+			QTEPlaying = false;
+			if(Input.GetKeyDown(KeyCode.E)){
+				done1 = false;
+				QTEPlaying = true;
+			}
+		}
+	}
+
+	void E6(){
+		QTENum.text = QTE6.ToString();
+		QTEKey.text = "[A]";
+
+		if(Input.GetKeyDown(KeyCode.A) && QTE6 > 0f){
+			done6 = true;
+		}
+
+		if(Input.GetKeyDown(KeyCode.A) && QTE6 <= 0f){
+			QTEKey.text = "Fail, try again... Press 'E'";
+			QTEPlaying = false;
+			if(Input.GetKeyDown(KeyCode.E)){
+				done1 = false;
+				QTEPlaying = true;
+			}
+		}
+	}
+
+	void E7(){
+		QTENum.text = QTE7.ToString();
+		QTEKey.text = "[V]";
+
+		if(Input.GetKeyDown(KeyCode.V) && QTE7 > 0f){
+			done7 = true;
+		}
+
+		if(Input.GetKeyDown(KeyCode.V) && QTE7 <= 0f){
+			QTEKey.text = "Fail, try again... Press 'E'";
+			QTEPlaying = false;
+			if(Input.GetKeyDown(KeyCode.E)){
+				done1 = false;
+				QTEPlaying = true;
+			}
+		}
+		
+	}
+
+	public void OnTriggerStay(Collider other){
+		if(other.gameObject.tag == "Player"){
+
 			if(Input.GetKeyDown(KeyCode.F)){
-					QTE1Start = true;
+					interactUI.SetActive(false);
+					QTEPlaying = true;
             }
 		}
 	}
-			public void OnTriggerExit(Collider other)
-			{
-		if(other.gameObject.tag == "Player")
-		{
-				interactUI.SetActive(false);
-			}
+
+	public void OnTriggerEnter(Collider other){
+		if(other.gameObject.tag == "Player"){
+			interactUI.SetActive(true);
 		}
+	}
+	
+	public void OnTriggerExit(Collider other){
+		if(other.gameObject.tag == "Player"){
+			interactUI.SetActive(false);
+		}
+	}
 }	
