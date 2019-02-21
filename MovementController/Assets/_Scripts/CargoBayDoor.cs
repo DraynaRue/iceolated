c80697cdc48f795d2b01b6a2d87b6167bb0a13ac
Mika Joel <me@mikajoel.fyi>
2019-02-21 21:32:48 +0000

    Mission Brief Added

---
+++ MovementController/Assets/_Scripts/CargoBayDoor.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CargoBayDoor : MonoBehaviour {
+
+	private Animation anim;
+
+	void Start(){
+		anim = GetComponent<Animation>();
+	}
+
+	public void OpenDoor(){
+		anim["Door"].time = 2.2f;
+	}
+}
--- MovementController/Assets/_Scripts/OpeningScene/CAM.cs
+++ MovementController/Assets/_Scripts/OpeningScene/CAM.cs
@@ -7,6 +7,9 @@ public class CAM : MonoBehaviour {
 	public GameObject pod;
 	public GameObject pos1, pos2;
 	public GameObject canvas;
+	public GameObject screenText;
+	public bool point2 = false;
+	public DialougeIntro dialogueIntro;
 
 	float t;
 	float timetoreach = 6f;
@@ -16,7 +19,9 @@ public class CAM : MonoBehaviour {
 	void Start(){
 		cam = gameObject.GetComponent<Camera>();
 		canvas.SetActive(false);
+		dialogueIntro = this.GetComponent<DialougeIntro>();
 	}
+	
 	void Update () {
 		t += Time.deltaTime / timetoreach;
 		Vector3 targetDir = pod.transform.position - transform.position;
@@ -27,19 +32,15 @@ public class CAM : MonoBehaviour {
 
 		transform.position = Vector3.Lerp(pos1.transform.position, pos2.transform.position, t);
 
-		if(transform.position == pos2.transform.position){
-			StartCoroutine(CAMMOVE());
+		if(transform.position == pos2.transform.position && point2 == false){
+			point2 = true;
+			screenText.SetActive(true);
+			dialogueIntro.GO();
 		}
 
-		if(Input.GetKeyDown(KeyCode.Escape)){
+		if(Input.GetKeyDown(KeyCode.Return)){
 			cam.depth = -1f;
 			canvas.SetActive(true);
 		}
-	}
-
-	IEnumerator CAMMOVE(){
-		yield return new WaitForSeconds(4f);
-		cam.depth = -1f;
-		canvas.SetActive(true);
 	}
 }
