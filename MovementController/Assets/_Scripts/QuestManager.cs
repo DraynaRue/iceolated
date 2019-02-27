46d015142c3b5b4f1d8c2f182dce85b3e46c9b8d
DraynaRue <mattie0@hotmail.co.uk>
2019-02-27 11:19:47 +0000

    Started QuestManager

---
+++ MovementController/Assets/_Scripts/QuestManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestManager : MonoBehaviour 
+{
+	public QuestObjective[] quests;
+	public bool[] questCompleted;
+	// Use this for initialization
+	void Start () 
+	{
+		questCompleted = new bool[quests.Length];
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+}
---
+++ MovementController/Assets/_Scripts/QuestObjective.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QuestObjective : MonoBehaviour {
+	public int questNumber;
+	public QuestManager theQM;
+	public Text questText;
+	public string questString;
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+
+	public void StartQuest ()
+	{
+		questText.text = questString;
+	}
+	public void EndQuest ()
+	{
+		questText.text = "";
+		theQM.questCompleted[questNumber] = true;
+		gameObject.SetActive(false);
+	}
+}
