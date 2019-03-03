67846831935faa46e49677e06b85a8fd8b9673be
marcushill <marcushill835@gmail.com>
2019-03-03 15:47:52 +0000

    Asset's and Audio Log 1 Working

---
+++ MovementController/Assets/_Scripts/Audio_Logs.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Audio_Logs : MonoBehaviour 
+{
+	 AudioSource audioLog;
+
+	void Start(){
+		audioLog = GetComponent<AudioSource>();
+	}
+
+	void OnTriggerStayS (Collider other)
+	{
+		if (other.gameObject.tag == "Player")
+		{
+			if (Input.GetKeyDown (KeyCode.F)) 
+			{
+				Debug.Log ("In");
+				audioLog.Play ();
+				//Destroy (this.gameObject);
+			}
+		}
+	}
+}
