e73d6c3e16244e0a4c81b216e5b4141d12ae4261
mj <mj>
2019-03-13 10:44:12 +0000

    map added and added server inv icon

---
+++ MovementController/Assets/_Scripts/LevelMap.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelMap : MonoBehaviour {
+
+	public GameObject map;
+	public bool mapObtained = false;
+
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.M) && mapObtained == true){
+			map.SetActive(true);
+		}
+	}
+
+	public void ObtainMap(){
+		mapObtained = true;
+	}
+}
