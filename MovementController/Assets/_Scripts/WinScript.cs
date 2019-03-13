fc09983b7e24f4defcd655ceaffce98039243b58
DraynaRue <mattie0@hotmail.co.uk>
2019-03-13 10:38:36 +0000

    Win Script Finished. Needs Implementing In Master

--- MovementController/Assets/_Scripts/WinScript.cs
+++ MovementController/Assets/_Scripts/WinScript.cs
@@ -1,16 +1,52 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class WinScript : MonoBehaviour {
-
+public class WinScript : MonoBehaviour
+{
+	public GameObject InteractUI;
+	public Image ServerInvIcon;
 	// Use this for initialization
-	void Start () {
+	void Start ()
+	{
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+	{
 		
+	}
+	void OnTriggerEnter(Collider other) 
+	{
+		if (InteractUI != null)
+		{
+			InteractUI.SetActive(true);
+		}
+	}
+	void OnTriggerStay(Collider other)
+	{
+		if (Input.GetAxis("Interact") > 0)
+		{
+			if (ServerInvIcon != null)
+			{
+				if (ServerInvIcon.gameObject.activeSelf == true)
+				{
+					Debug.Log("Congratulations you have completed the game! Thanks for playing!");
+					if (InteractUI != null)
+					{
+						InteractUI.SetActive(false);
+					}
+				}
+			}
+		}
+	}
+	void OnTriggerExit(Collider other)
+	{
+		if (InteractUI != null)
+		{
+			InteractUI.SetActive(false);
+		}
 	}
 }
