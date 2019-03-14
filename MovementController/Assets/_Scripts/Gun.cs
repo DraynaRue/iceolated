92430a8b5609b737492d9fe9edaa109e43270c8c
Mika Joel <me@mikajoel.fyi>
2019-03-14 00:10:42 +0000

    Revert "Revert "ice break particles""
    This reverts commit b8785ee9c89643306ad3357ad6d1d0c277c8c588.

--- MovementController/Assets/_Scripts/Gun.cs
+++ MovementController/Assets/_Scripts/Gun.cs
@@ -6,7 +6,7 @@ using UnityEngine.UI;
 public class Gun : MonoBehaviour 
 {
 
-	public GameObject laser;
+	public GameObject laser, iceBreakParticle;
 	public Animation gun;
 	Turret turret;
 	public Text hitPoint;
@@ -14,6 +14,7 @@ public class Gun : MonoBehaviour
 
 	public static bool isShooting = false;
 	public static bool isMeltingIce = false;
+	
 	void Start(){
 		fuelHolder.GetComponent<Fuel>();
 	}
@@ -47,6 +48,7 @@ public class Gun : MonoBehaviour
 
 				if (currentProgress >= 1.0f)
 				{
+					Instantiate(iceBreakParticle, hit.transform.position, Quaternion.identity);
 					Destroy(hit.transform.gameObject);
 				}
 			} 
