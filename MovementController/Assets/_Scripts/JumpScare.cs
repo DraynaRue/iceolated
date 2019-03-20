cefefb898a329c2194e657df6646420144572417
m <m>
2019-03-20 10:03:13 +0000

    boo

---
+++ MovementController/Assets/_Scripts/JumpScare.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpScare : MonoBehaviour {
+
+    public Animator anim;
+    // Use this for initialization
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            anim.SetTrigger("Scare");
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            anim.SetTrigger("Idle");
+        }
+    }
+}
