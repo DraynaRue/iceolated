82be42eb17d183aef0c306a333e16138048692a1
m <m>
2019-03-25 11:47:54 +0000

    Ice room ambience

---
+++ MovementController/Assets/_Scripts/Ice_Room_.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ice_Room_ : MonoBehaviour 
+{
+ void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            AudioManager.Instance.PlaySound ("IceRoom", transform.position);
+        }
+    }
+}
